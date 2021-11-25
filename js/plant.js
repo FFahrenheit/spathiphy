e7357d846bfb1f87a99b35c04c6ef618f720cdc8
Iván L. Murillo <diskman199@gmail.com>
2021-11-24 20:20:30 -0600

    Vista de plantita

---
+++ js/plant.js
@@ -0,0 +1,35 @@
+let id;
+
+const form = document.getElementById('form');
+
+const planta = {
+    nombre: document.getElementById('nombre'),
+    utila: document.getElementById('ultima'),
+    temperatura: document.getElementById('temperatura'),
+    luminosidad: document.getElementById('luminosidad'),
+    humedad: document.getElementById('humedad'),
+    max_temp: document.getElementById('max_temp'),
+    min_temp: document.getElementById('min_temp'),
+    max_hum: document.getElementById('max_hum'),
+    min_hum: document.getElementById('min_hum')
+};
+
+
+(() => {
+    const queryString = window.location.search;
+    console.log({ queryString });
+
+    if(queryString == ''){
+        window.location.assign('dashboard.html');
+    }else{
+        const urlParams = new URLSearchParams(queryString);
+        id = urlParams.get('id');
+        loadInfo();
+    }
+})();
+
+
+function loadInfo(){
+    console.log({id});
+    // fetch('').
+}
