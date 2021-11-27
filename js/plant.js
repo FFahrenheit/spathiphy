e9d9cc4368a9ff46e06781ceb9f93c5dfdf03eb4
Iván L. Murillo <diskman199@gmail.com>
2021-11-26 20:57:00 -0600

    Dashboard de plantita y set points

--- js/plant.js
+++ js/plant.js
@@ -1,10 +1,11 @@
 let id;
+let set = false;
 
 const form = document.getElementById('form');
 
 const planta = {
     nombre: document.getElementById('nombre'),
-    utila: document.getElementById('ultima'),
+    ultima: document.getElementById('ultima'),
     temperatura: document.getElementById('temperatura'),
     luminosidad: document.getElementById('luminosidad'),
     humedad: document.getElementById('humedad'),
@@ -24,18 +25,78 @@ const planta = {
     }else{
         const urlParams = new URLSearchParams(queryString);
         id = urlParams.get('id');
-        loadInfo();
+        setInterval(loadInfo, 2000);
     }
 })();
 
+
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
-
     console.log('Saving...');
-})
+
+    const xForm = 
+    `max_temp=${planta.max_temp.value}\
+&min_temp=${planta.min_temp.value}\
+&min_hum=${planta.min_hum.value}\
+&max_hum=${planta.max_hum.value}`;
+    
+    console.log(xForm);
+
+    fetch(base_url + '/planta/settings/' + id,
+    {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        },
+        body: xForm
+    })
+    .then(r => r.json())
+    .then(resp=>{
+        if(resp['acknowledged']){
+            alertar('Datos cambiados', 'success');
+        }else{
+            alertar('No se pudieron guardar los datos', 'danger');
+        }
+    })
+    .catch(error=>{
+        console.error(error);
+        alertar('No se pudieron guardar los datos', 'danger');
+    });
+});
 
 
 function loadInfo(){
     console.log({id});
-    // fetch('').
+    const token = 'Bearer ' + localStorage.getItem('token');
+
+    fetch(base_url + '/planta/' + id,
+    {
+        method: 'GET',
+        headers: {
+            'Authorization': token
+        }
+    })
+    .then(r => r.json())
+    .then(resp => {
+        console.log(resp);
+        const last = resp['last_rec'];
+
+        planta.nombre.innerHTML = 'Planta #' + resp['id_micro'];
+        planta.ultima.innerHTML = new Date(last['recTime']);
+        planta.temperatura.innerHTML = last['temperatura'] + '°C';
+        planta.humedad.innerHTML = last['humedad'] + '%';
+        planta.luminosidad.innerHTML = last['luminosidad'] + ' lx';
+
+        if(!set){
+            planta.max_temp.value = resp['max_temp'];
+            planta.min_temp.value = resp['min_temp'];
+            planta.max_hum.value = resp['max_hum'];
+            planta.min_hum.value = resp['min_hum'];
+            set = true;
+        }
+
+    })
+    .catch(error=>{
+        alertar('Error al recibir información', 'danger');
+    });
 }
